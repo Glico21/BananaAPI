8d7b924d0e79d2eb2b1f40e0ecbf142ac2e86c32
dmitriy-dev <21glicodin@gmail.com>
2022-07-19 18:38:01 +0400

    Add palm schema

--- application/schemas.py
+++ application/schemas.py
@@ -1,6 +1,6 @@
 from flask_marshmallow import Marshmallow
 
-from application.models import Banana
+from application.models import Banana, Palm
 
 ma = Marshmallow()
 
@@ -11,5 +11,14 @@ class BananaSchema(ma.SQLAlchemyAutoSchema):
         load_instance = True
 
 
+class PalmSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Palm
+        load_instance = True
+
+
 banana_schema = BananaSchema()
 bananas_schema = BananaSchema(many=True)
+
+palm_schema = PalmSchema()
+palms_schema = PalmSchema(many=True)
